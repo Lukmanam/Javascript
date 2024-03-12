95eeb460721ba24b5639871a702b6584d123fd29
Lukmanam <lukkuptrklm2000@gmail.com>
2024-03-12 08:00:52 +0530

    js

--- Boarding/callback.js
+++ Boarding/callback.js
@@ -1,11 +1,16 @@
-function sum(a,b,multiple)
-{
-let result=a+b;
-console.log(result,"sum");
+// function sum(a,b,multiple)
+// {
+// let result=a+b;
+// console.log(result,"sum");
+// multiple(a,b)
+// }
 
-}
+// function multiple(a,b){
+//     console.log("multiple is",a*b);
 
-function multiple(a,b){
-    console.log("multiple is",a*b);
+// }
 
-}
+// let a=100;
+// let b=20
+// sum(a,b,multiple)
+
